cfc0da3a5dd8da45e029caae4a541e986549efaa
Hendry Khoza <h3khoza@gmail.com>
2020-07-28 23:15:49 +0200

    updated site details

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Hendry Khoza's Portfolio`,
+    description: `Official website / blog of Hendry Khoza`,
+    author: `Hendry Khoza`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
